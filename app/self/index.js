1498dd8a75dc48866bca30abb4f2e59ab15e60d6
Gautam Arora <gautamsarora3@gmail.com>
2016-01-22 20:31:53 -0500

    Updating header to pass topbar title

--- app/self/index.js
+++ app/self/index.js
@@ -1,7 +1,9 @@
 var core = require('@gautamarora/core');
+var header = require('@gautamarora/header');
 
 module.exports.init = function (app) {
   console.log('blog self init');
   core.registerRoutes(__dirname, 'routes', app);
   core.registerPartials(__dirname, 'partials', 'gallery');
+  header.setInitialState({"topbar":{"title": "My Blog"}});
 };
